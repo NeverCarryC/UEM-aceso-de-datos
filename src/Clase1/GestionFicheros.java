edfed22119d6a1fae4433568748e80fadae1df46
NeverCarryC <nishangzhi95@gmail.com>
2024-12-24 07:26:28 +0100

    test2

--- src/Clase1/GestionFicheros.java
+++ src/Clase1/GestionFicheros.java
@@ -1,6 +1,7 @@
 package Clase1;
 
 import java.io.File;
+import java.io.IOException;
 
 public class GestionFicheros {
 
@@ -8,12 +9,21 @@ public class GestionFicheros {
         // file es logico, no es fisico
         // File file = new File("src/Clase1/resources/ejemplo.txt");
         File file = new File("src/Clase1/resources/ejemplo1.txt");
-        System.out.println(file.getName());
-        System.out.println(file.getAbsolutePath());
-        System.out.println(file.canRead());
-        System.out.println(file.canWrite());
-        System.out.println(file.isFile());
-        System.out.println(file.isHidden());
+
+        if(!file.exists()){
+            try {
+                file.createNewFile();// logico -> fisico
+                System.out.println(file.getName());
+                System.out.println(file.getAbsolutePath());
+                System.out.println(file.canRead());
+                System.out.println(file.canWrite());
+                System.out.println(file.isFile());
+                System.out.println(file.isHidden());
+            } catch (IOException e) {
+                // throw new RuntimeException(e);
+                System.out.println("Error en la creación del fichero");
+            }
+        }
     }
 
     public static void main(String[] args) {
